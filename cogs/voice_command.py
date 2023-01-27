7f41af76925be7920a50bdc564850ab19af526dd
Sylvester Alderson <51517881+SylvesterFox@users.noreply.github.com>
2023-01-27 18:49:05 +0300

    Прототип системы для создание приватных гс

---
+++ cogs/voice_command.py
@@ -0,0 +1,31 @@
+import discord
+from settings_bot import config
+from discord.ext import commands
+from discord import app_commands
+from database import VcDB
+
+
+class VoiceCommand(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        self.vc_db = VcDB()
+
+    @app_commands.command(name="vcsetup")
+    @app_commands.checks.has_permissions(administrator=True)
+    async def voice_setup(self, interaction: discord.Interaction):
+        if self.vc_db.get_lobby_from_guild(guild_id=interaction.guild.id) is not None:
+            print(self.vc_db.get_lobby_from_guild(guild_id=interaction.guild.id))
+            return await interaction.response.send_message("You already have a lobby")
+        mbed = discord.Embed(
+            title="Success!",
+            description=f"A lobby has been created on your server to create voice channels ✅",
+            color=discord.Color.green()
+        )
+        vc = await interaction.guild.create_voice_channel(name="[+] Create Voice Channel [+]")
+        await interaction.response.send_message(embed=mbed)
+        self.vc_db.vc_setup_insert(guild_id=interaction.guild.id, voice_channel_id=vc.id)
+
+
+async def setup(bot: commands.Bot):
+    settings = config()
+    await bot.add_cog(VoiceCommand(bot), guilds=[discord.Object(id=settings['main_guild'])])
---
+++ cogs/voice_event.py
@@ -0,0 +1,40 @@
+import discord
+from database import VcDB
+from discord.ext import commands
+
+
+class VoiceEvent(commands.Cog):
+    def __int__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, after):
+
+        if after.channel is not None and before.channel is None:
+            vc_db = VcDB()
+            category = None
+            if after.channel.category_id is not None:
+                category = discord.utils.get(member.guild.categories, id=after.channel.category_id)
+
+            if vc_db.get_lobby_from_guild(guild_id=member.guild.id) == after.channel.id:
+                vc = await member.guild.create_voice_channel(name=f"Voice Lair {member.display_name}",
+                                                             category=category)
+                vc_db.create_vc(vc_id=vc.id,
+                                name=vc.name,
+                                created_at=member.display_name,
+                                created_member_id=member.id,
+                                guild_id=member.guild.id)
+                await member.move_to(vc)
+        elif before.channel is not None and after.channel is None:
+            vc_db = VcDB()
+            voice_channel = discord.utils.get(member.guild.channels, id=before.channel.id)
+            members_count = len(voice_channel.members)
+            if members_count == 0:
+                name = vc_db.get_vcdb_name(channel_id=before.channel.id, guild_id=member.guild.id)
+                if name is not None:
+                    vc_db.delete_vc(channel_id=before.channel.id, guild_id=member.guild.id)
+                    await voice_channel.delete()
+
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(VoiceEvent(bot))
